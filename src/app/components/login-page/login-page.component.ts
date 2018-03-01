fc8a517b7090ae1b07e3695eae739af88a2a5858
Sandro Santander <sandrosm06@gmail.com>
2018-03-01 17:08:58 -0500

    Add Login/Logout

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
+  public email:string;
+  public password: string;
 
-  constructor() { }
+  constructor(
+    private _authService: AuthService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+  }
+  
+  onSubmitLogin(){
+    this._authService.loginEmail(this.email, this.password)
+    .then( (res) => {
+      this._router.navigate(['/private']);
+    }).catch( (err) => {
+      console.log(err);
+      this._router.navigate(['/login']);
+    });
   }
 
 }
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  public isLogin:boolean;
+  public username:string;
+  public emailUser:string;
+
+  constructor(
+    private _authService: AuthService,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
-  }
+    this._authService.getAuth().subscribe( auth => {
+      if(auth){
+        this.isLogin = true;
+        this.username = auth.displayName;
+        this.emailUser = auth.email;
+        
+      }else{
+        this.isLogin = false;
+      }
+    })
 
+  }
+  onClickLogOut(){
+    this._authService.logout();
+    
+  }
 }
